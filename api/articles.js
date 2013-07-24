21d98d382955af70852a82d312164bdb8ffe5fad
stavinski_2000@yahoo.com <stavinski_2000@yahoo.com>
2013-07-24 21:43:12 +0100

    API changes
    Explicit open/close DB
    Ordering of articles

--- api/articles.js
+++ api/articles.js
@@ -2,78 +2,94 @@ var config = require('../config')
     , engine = require('tingodb')()
     , db = new engine.Db( config.db.path , {})
     , Q = require('q')
-    , logger = require('../logger')
-    , subscriptions = db.collection('subscriptions')
-    , articles = db.collection('articles');
-
+    , logger = require('../logger');
+    
 exports.getArticles = function(req, res) {
     var filter = {
         profile: config.profiles.id
     };
     
     var fetchFromCursor = function (cursor) {
-        return Q.nbind(cursor.toArray, cursor)();
+        return Q.ninvoke(cursor, 'toArray');
     };
     
     var flattenArticles = function (articles) {
-        var flattened = [];
-        return flattened.concat.apply(flattened, articles);
-    };
-           
-    Q.nbind(subscriptions.find, subscriptions)({ profile : config.profiles.id })
-        .then(fetchFromCursor)
-        .then(function (subs) { 
-            var deferreds = [];
-                        
-            subs.forEach(function (sub) {
-                var deferred = Q.defer();
-                
-                var filter = { 
-                    subscription : sub._id,
-                };
-                
-                if ((!req.query.read) && (!req.query.starred)) filter.read = false;
-                if (req.query.read) filter.read = true;
-                if (req.query.starred) filter.starred = true;
-                
-                Q.nbind(articles.find, articles)(filter, {content:0})
-                    .then(fetchFromCursor)
-                    .then(deferred.resolve)
-                    .then(deferreds.push(deferred.promise));
-            }); 
-
-            return Q.all(deferreds);
-        })
-        .then(flattenArticles)
-        .then(function (results) { res.json(results); })
-        .done();
+        var flattened = []
+            , results = flattened.concat.apply(flattened, articles);
         
+        results.sort(function (first, second) {
+            return second.published - first.published;
+        });
+        
+        return results;
+    };
+    
+    Q.ninvoke(db, 'open')
+        .then (function (db) {
+            var   subscriptions = db.collection('subscriptions')
+                , articles = db.collection('articles');
+            Q.ninvoke(subscriptions, 'find', { profile : config.profiles.id })
+                .then(fetchFromCursor)
+                .then(function (subs) { 
+                    var deferreds = [];
+                                
+                    subs.forEach(function (sub) {
+                        var deferred = Q.defer();
+                        
+                        var filter = { 
+                            subscription : sub._id,
+                        };
+                        
+                        if ((!req.query.read) && (!req.query.starred)) filter.read = false;
+                        if (req.query.read) filter.read = true;
+                        if (req.query.starred) filter.starred = true;
+                                                
+                        Q.invoke(articles, 'find', filter, {content:0})
+                            .then(fetchFromCursor)
+                            .then(deferred.resolve)
+                            .then(deferreds.push(deferred.promise));
+                    }); 
+
+                    return Q.all(deferreds);
+            })
+            .then(flattenArticles)
+            .then(function (results) { res.json(results); })
+        })
+        .done();
+            
 };
 
 exports.getArticle = function(req, res) {
     var id = req.params.id;
-    articles.findOne({_id : id}, function(err, article) {
-        if (err) {
-            logger.error('articlesapi', 'raised when finding individual article', err);
-            return;
-        }
-        
-        res.json(article);
-    });
+    Q.ninvoke(db, 'open')
+        .then(function (db) {
+            return db.collection('articles');
+        })
+        .then(function (articles) {
+            return Q.ninvoke(articles, 'findOne', {_id : id});
+        })
+        .then(function (article) {
+            res.json(article);
+        })
+        .done();
 };
 
 exports.update = function (req, res) {
-    
-    return Q.ninvoke(articles, "findOne", { _id : req.params.id })
-        .then(function (result) {
-            var read = (req.body.read == "true");
-            var starred = (req.body.starred == "true");
-            
-            articles.update({ _id : result._id }, { $set: { read : read, starred : starred } });
-                        
-            if ((req.body.read) && (!result.read))
-                subscriptions.update({ _id: result.subscription }, { $inc : { unread : -1 } });            
-        }) 
+    Q.ninvoke(db, 'open')
+        .then(function (db) {
+            var   articles = db.collection('articles')
+                , subscriptions = db.collection('subscriptions');
+            Q.ninvoke(articles, "findOne", { _id : req.params.id })
+                .then(function (result) {
+                    var read = (req.body.read == "true");
+                    var starred = (req.body.starred == "true");
+                    
+                    articles.update({ _id : result._id }, { $set: { read : read, starred : starred } });
+                                
+                    if ((req.body.read) && (!result.read))
+                        subscriptions.update({ _id: result.subscription }, { $inc : { unread : -1 } });            
+                });
+        })
         .fin(function () { res.end() })
         .done();
 };
--- api/subscriptions.js
+++ api/subscriptions.js
@@ -1,24 +1,32 @@
 var config = require('../config')
     , engine = require('tingodb')()
     , db = new engine.Db( config.db.path , {})    
-    , subscriptions = db.collection('subscriptions');
+    , Q = require('q');
 
 exports.getSubscriptions = function(req, res){
-    subscriptions.find({ profile: config.profiles.id }).toArray(function (err, subscriptions) {
-        if (err) throw err;
-                
-        res.json(subscriptions);
-        res.end();
-    });
+    Q.ninvoke(db, 'open')
+        .then(function (db) {
+            return db.collection('subscriptions');
+        })
+        .then(function (subscriptions) {
+            var cursor = subscriptions.find({ profile: config.profiles.id });
+            return Q.ninvoke(cursor, 'toArray');
+        })
+        .then(function (subscriptions) {
+            res.json(subscriptions);
+        })
+        .done();
 };
 
 exports.getSubscription = function(req, res) {
     var id = req.params.id;
-    subscriptions.findOne({ _id : id }, function (err, subscription) {
-        if (err) throw err;
-        
-        res.json(subscription);
-        res.end();
-    });
-    
+    Q.ninvoke(db, 'open')
+        .then(function (db) {
+            var subscriptions = db.collection('subscriptions');
+            return Q.ninvoke(subscriptions, 'findOne', { _id : id });
+        })
+        .then(function (subscription) {
+            res.json(subscription);
+        })
+        .done();
 };
