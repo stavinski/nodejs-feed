1de69a8479bc298d44593446cf4827d286f7efe9
stavinski_2000@yahoo.com <stavinski_2000@yahoo.com>
2013-10-12 16:21:16 +0100

    category UI work

--- public/app/viewmodels/subscriptions.js
+++ public/app/viewmodels/subscriptions.js
@@ -1,7 +1,45 @@
 define(['plugins/router', 'knockout', 'connection', 'cache', 'jquery', 'contexts/subscriptions', 'contexts/articles', 'uri', 'fastclick'], function(router, ko, connection, cache, $, subscriptionsContext, articlesContext, uri, fastclick) {
-   
-    var bindSubscription = function (model) {
-          
+
+    var bindCategory = function (category) {
+        return {
+                    id : category.replace(/\s/g,'_'),
+                    title : category,
+                    collapsed : ko.observable(true),
+                    subscriptions : subscriptionsContext.subscriptions()
+                                        .filter(function (subscription) {
+                                            return subscription.category == category;
+                                        })
+                                        .map(bindSubscription),
+                    toggleCollapse : function () {
+                        this.collapsed(!this.collapsed());
+                        return true;
+                    },
+                    unread : ko.computed(function () {
+                        var self = category
+                            , subscriptionIds = subscriptionsContext.subscriptions()
+                                                .filter(function (subscription) {
+                                                    return subscription.category == category;
+                                                })
+                                                .map(function (subscription) {
+                                                    return subscription._id;    
+                                                });
+                        
+                        var count = subscriptionIds.map(function (id) {
+                                        var results = articlesContext.articles().filter(function (article) {
+                                                        return ((article.subscription == id) && (!article.read()))        
+                                                    });
+                                        return results.length;
+                                    })
+                                    .reduce(function (prev, next) {
+                                        return prev + next;    
+                                    });
+                        
+                        return (count > 0) ? count : '';                         
+                    })
+        };
+    };
+    
+    var bindSubscription = function (model) {  
         model.unread = ko.computed(function () {
             var self = model;
             var results = articlesContext.articles().filter(function (article) {
@@ -12,9 +50,7 @@ define(['plugins/router', 'knockout', 'connection', 'cache', 'jquery', 'contexts
         
         if (router.activeItem().subscription)        
             model.active = ko.observable(router.activeItem().subscription() == model._id);
-        
-        model.domain = uri(model.favicon).host;
-                
+                        
         return model;
     };
     
@@ -34,16 +70,7 @@ define(['plugins/router', 'knockout', 'connection', 'cache', 'jquery', 'contexts
         },
         categories : ko.computed(function () {
             var categories = subscriptionsContext.categories();
-            return categories.map(function (category) {
-                return {
-                    title : category,
-                    subscriptions : subscriptionsContext.subscriptions()
-                                        .filter(function (subscription) {
-                                            return subscription.category == category;
-                                        })
-                                        .map(bindSubscription)
-                };
-            });
+            return categories.map(bindCategory);
         })
     };
     
